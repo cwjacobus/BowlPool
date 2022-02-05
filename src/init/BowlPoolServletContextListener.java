e70b97a9430577773789c26b267008bd7defb4aa
Chris Jacobus <cjacobus65@gmail.com>
2022-02-05 11:51:41 -0600

    get DB info from props

--- src/init/BowlPoolServletContextListener.java
+++ src/init/BowlPoolServletContextListener.java
@@ -1,15 +1,26 @@
 package init;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.servlet.ServletContext;  
 import javax.servlet.ServletContextEvent;  
 import javax.servlet.ServletContextListener;  
 public class BowlPoolServletContextListener implements ServletContextListener{  
       public void contextInitialized(ServletContextEvent event) {  
             ServletContext context = event.getServletContext();  
-            String url = context.getInitParameter("URL");  
-            String database=context.getInitParameter("Database name");  
-            String username=context.getInitParameter("Username");  
-            String password=context.getInitParameter("password");  
+            Properties prop = new Properties();
+            try {
+            	InputStream input = context.getResourceAsStream("/WEB-INF/BowlPool.properties");
+            	prop.load(input);
+            }
+            catch (Exception e) {
+            	e.printStackTrace();
+            }
+            String url = prop.getProperty("db.url"); //context.getInitParameter("URL");  // from web.xml
+            String database = prop.getProperty("db.database");  
+            String username = prop.getProperty("db.username");  
+            String password = prop.getProperty("db.password");  
             BowlPoolDatabase mydb  = new BowlPoolDatabase(url+database, username, password);  
             context.setAttribute("Database", mydb); 
             System.out.println("context initialized - DB Info: " + url + database + " " + username);
